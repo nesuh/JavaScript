f88c773d4aa15cad2d45e23ee9eb67e3902af4d4
antenhe sileshi <antenhesileshi@gmail.com>
2024-03-08 22:55:14 -0800

    my javascript project is commited

---
+++ app.js
@@ -0,0 +1,32 @@
+function fahr(Tf){
+let Tc=(Tf-32)*5/9;
+
+console.log(Tc);
+
+}
+function cels(Tc){
+    let Tf=(Tc*9/5)+32
+  console.log(Tf);
+   
+}
+
+cels(25);
+fahr(86);
+cels(-5);
+
+
+let tcf=document.getElementById('num').value;
+console.log(tcf);
+
+
+
+
+
+
+
+
+{
+  "name":"antenhe",
+  "age":"12";
+}
+
